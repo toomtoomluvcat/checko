4832aa5b5bcd781a45d8fc3d27580907255191cb
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-12-06 15:34:48 +0700

    frontend setup

---
+++ app/component/nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+        <nav >
+            <ul className='flex sm:justify-center gap-x-[15px] m-[15px]'>
+                <li>Home</li>
+                <li>Form</li>
+                <li>How to use</li>
+                <li>Help & support</li>
+            </ul>
+           
+        </nav>
+    </div>
+    
+  )
+}
+
+export default Nav
