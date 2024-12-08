f584d6949745905bc844ead4513f240d7ca50aaa
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-12-08 12:44:21 +0700

    finish form

--- app/form/page.jsx
+++ app/form/page.jsx
@@ -353,33 +353,7 @@ function Form() {
 
   return (
     <div className="flex flex-col">
-        <nav className="relative bg-white z-index-40">
-          <ul className=" font-kanit font-medium my-[30px] flex justify-end relative right-[20px] md:justify-center gap-x-[7px] md:gap-x-[90px]">
-            <Link href="/">
-              <li className="hover:text-[#5a5a5a] transition-all duration-300 ease-in-out">
-                Home
-              </li>
-            </Link>
-            <Link href="/form">
-              <li className="hidden md:block hover:text-[#5a5a5a] transition-all duration-300 ease-in-out">
-                Form
-              </li>
-            </Link>
-            <Link href="/howToUse">
-              <li className="hidden md:block hover:text-[#5a5a5a] transition-all duration-300 ease-in-out">
-                How to use
-              </li>
-            </Link>
-            <Link
-              target="blank"
-              href="https://physics.sc.kku.ac.th/?page_id=727"
-            >
-              <li className="hover:text-[#5a5a5a] transition-all duration-300 ease-in-out">
-                Help & support
-              </li>
-            </Link>
-          </ul>
-        </nav>
+        <Nav></Nav>
       <NextImage
         className="w-[100px] sm:top-[70px] sm:left-[100px] top-[25px] left-[30px] absolute"
         width={650}
