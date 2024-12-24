16f5a594a07232024d8b17a98e2595229f1c0cc9
Toomtoomluvcat <kritsada.ba@kkumail.com>
2024-12-24 09:40:42 +0700

    add submit history

--- app/component/nav.jsx
+++ app/component/nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
                 <Link href='/'><li className='hover:text-[#5a5a5a] transition-all duration-300 ease-in-out'>Home</li></Link>
                 <Link href='/form'><li className='hidden md:block hover:text-[#5a5a5a] transition-all duration-300 ease-in-out'>Form</li></Link>
                 <Link href='/howToUse'><li className='hidden md:block hover:text-[#5a5a5a] transition-all duration-300 ease-in-out'>How to use</li></Link>
-                <Link target='blank' href='https://physics.sc.kku.ac.th/?page_id=727'><li className='hover:text-[#5a5a5a] transition-all duration-300 ease-in-out'>Help & support</li></Link>
+                <Link target='blank' href='https://docs.google.com/spreadsheets/d/1aOklifcp5SNvkdTZBSN-lIkj7ZpEEYpzRKiTcB9lnXg/edit?usp=sharing'><li className='hover:text-[#5a5a5a] transition-all duration-300 ease-in-out'>Submit history</li></Link>
             </ul>
         </nav>
     </div>
