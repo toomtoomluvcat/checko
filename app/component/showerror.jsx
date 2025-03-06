848d59a8f4d5dd04ce010dae03545ba37b053875
Toomtoomluvcat <kritsada.ba@kkumail.com>
2025-03-06 11:13:38 +0700

    change algorithm

--- app/component/showerror.jsx
+++ app/component/showerror.jsx
@@ -8,7 +8,7 @@ function ShowError({ onclick }) {
       <div className="max-w-[470px] bg-white h-[550px] sm:h-[550px] flex fiexd inset-0 left-[50px] right-[50px] m-auto rounded-[20px] flex-col items-center absolute justify-center">
         <div className="relative w-[140px] h-[140px] sm:w-[170px] bottom-[50px] mb-[-90px] sm:mb-[-70px] sm:h-[170px]">
           <Image
-            src="/image/01.png"
+            src="/02.jpg"
             width={200}
             height={200}
             alt="catinbag"
@@ -17,11 +17,10 @@ function ShowError({ onclick }) {
         <div className="w-[100%] relative shadow-md border-b-[2px] border-gray h-10 "></div>
         <div className="flex flex-col justify-ceter items-center text-center mt-[40px] font-kanit space-y-[10px]">
           <h1 className="font-medium text-[20px] sm:text-[25px] mb-[10px] text-red-400">
-            เซิฟเว่อร์มีปัญหา
+           admin พิมตก
           </h1>
           <p className="sm:text-[15px] w-[70%] text-[14px] text-[#5B5B5B]">
-            เนื่องจากในวันที่ 4
-            มีนาคมที่ผ่านมาเกิดการเชื่อมต่อที่ผิดพลาดของเซิฟเว่อร์ ทำให้รายชื่อในการเช็คชื่ออาจจะมีการตกหล่นไปบ้างแอดมินจึงแก้ปัญหาโดยการเช็คชื่อให้กับ "ทุกคน" ในวันนั้นเรียบร้อยแล้ว จึงขออภัยในความไม่สะดวกครับ 
+           เนื่องจากในวันที่ 6 มีนาคมที่ผ่านมาไม่สามารถเช็คชื่อได้ เพราะแอดมินพิมเครื่องหมาย &gt; แทน &lt; ทำให้คนนอกห้องเช็คชื่อได้แทนคนในห้อง ตอนนี้แก้แล้ว ขออภัยมากๆ ครับ (แต่มีคนเช็คชื่อมานะ)
           </p>
           <button>
             <p
